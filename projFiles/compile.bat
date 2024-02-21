adf7d7722c1925c79e42d1f29b83b64e3c9eed01
Ava <121409172+DuskieWhy@users.noreply.github.com>
2024-02-20 23:14:06 -0500

    Fixed some library compilation errors

--- projFiles/compile.bat
+++ projFiles/compile.bat
@@ -11,8 +11,11 @@ haxelib set openfl 9.2.2
 haxelib install hscript
 haxelib set hxvlc
 haxelib set hxcpp 4.3.2
+haxelib remove linc_luajit
+haxelib remove discord_rpc
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
+haxelib install hxvlc 1.2.0
 echo BUILDING GAME
 lime test windows
 echo.
